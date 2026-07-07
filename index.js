19618e1bfd91890dfd1a99cec4a30574771e2491
donnyusmar <donnyusmar@gmail.com>
2026-07-07 16:00:31 +0700

    auto: update chatbot and style 20260707_160030

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import 'dotenv/config'; // Trigger rebuild for new netlify domain
 import express from 'express';
 import cors from 'cors';
 import { GoogleGenAI } from '@google/genai';
